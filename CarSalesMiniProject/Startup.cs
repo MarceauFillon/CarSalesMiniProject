e0598e863fdb3ef634a14952f1cb14b394107d1c
MarceauFillon <marceau-fillon@outlook.fr>
2020-09-16 02:21:59 +1000

    Embedded database in solution

--- CarSalesMiniProject/Startup.cs
+++ CarSalesMiniProject/Startup.cs
@@ -9,19 +9,38 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Server.IISIntegration;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
 
 namespace CarSalesMiniProject
 {
     public class Startup
     {
+        private string _contentRootPath = "";
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            _contentRootPath = env.ContentRootPath;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
             services.AddScoped<ICarRepository, CarRepository>();
+            services.AddAuthentication(IISDefaults.AuthenticationScheme);
+
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            if (connectionString.Contains("%CONTENTROOTPATH%"))
+            {
+                connectionString = connectionString.Replace("%CONTENTROOTPATH%", _contentRootPath);
+            }
+
             services.AddDbContext<VehiclesContext>(options => options.UseSqlServer(
-                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=C:\USERS\MARCEAU\SOURCE\REPOS\CARSALESMINIPROJECT\MINIPROJECTDB.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
+                connectionString,
+                providerOptions => providerOptions.EnableRetryOnFailure()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
